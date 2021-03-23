bb3a52530097fb8704d7b9957712523cd9e9da73
Krzysztof Musiał <niemusialem@gmail.com>
2021-03-23 09:17:24 +0100

    Added routes and components

--- frontend/warehouse-managment-system/src/app/app-routing.module.ts
+++ frontend/warehouse-managment-system/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  // {path: 'app', component: , children: [
+  //   {path: 'setup', component: },
+  //   {path: 'dashboard', component: },
+  //   {path: 'settings', component: },
+  //   {path: 'workers', component: },
+  //   {path: 'storage', component: },
+  //   {path: 'products', component: },
+  //   {path: 'actions', component: },
+  // ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
