d443fbbde4d3f55f39615778229c8a3df5777751
Krzysztof Musiał <niemusialem@gmail.com>
2021-03-23 11:37:28 +0100

    Added status component and styling assets.

--- frontend/warehouse-managment-system/src/app/app.module.ts
+++ frontend/warehouse-managment-system/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+// Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { StatusComponent } from './shared/components/status/status.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule,
